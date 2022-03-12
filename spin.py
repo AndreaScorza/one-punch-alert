0509bac7fbf856729843ceceb2a04277b3b46a1f
AndreaScorza <atlantas95@gmail.com>
2022-03-12 23:37:57 +0100

    Added a continuos configuration

---
+++ spin.py
@@ -0,0 +1,27 @@
+from pushbullet import Pushbullet
+import requests, time
+from bs4 import BeautifulSoup
+
+API_KEY = "o.XDaXmpNIpR1nNM2fMVNw8mXmXr1YiPB6"
+
+pb = Pushbullet(API_KEY)
+URL = "https://w4.onepunch-manga.com/"
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.text, features="html.parser")
+last_episode = soup.find(class_="su-post")
+title = last_episode.a.string
+
+while True:
+    time.sleep(20)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    last_episode = soup.find(class_="su-post")
+    new_title = last_episode.a.string
+
+    if new_title != title:
+        pb.push_note('New Chapter!!!', title + '\n' + URL)
+        title = new_title
+    else:
+        pb.push_note('Nothing ...', 'No new chapter today\n' + URL)
+
+
