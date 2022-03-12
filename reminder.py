95f996285b47a287e6f0aac4acce1928744249b6
AndreaScorza <atlantas95@gmail.com>
2022-03-12 19:33:39 +0100

    added the reminder code

---
+++ reminder.py
@@ -0,0 +1,30 @@
+'''from pushbullet import Pushbullet
+import requests
+from bs4 import BeautifulSoup
+
+API_KEY = "o.XDaXmpNIpR1nNM2fMVNw8mXmXr1YiPB6"
+file_name = "previous.txt"
+
+file = open(file_name, "r+")
+text = file.read()
+
+pb = Pushbullet(API_KEY)
+URL = "https://w4.onepunch-manga.com/"
+r = requests.get(URL)
+
+
+soup = BeautifulSoup(r.text, features="html.parser")
+last_episode = soup.find(class_="su-post")
+title = last_episode.a.string
+if text == title:
+    push = pb.push_note('Nothing ...', 'No new chapter today')
+else:
+    push = pb.push_note('New Chapter!!!', title)
+    file.seek(0)
+    file.truncate()
+    file.write(title)
+file.close()'''
+
+import requests
+URL = "https://w4.onepunch-manga.com/"
+print(requests.get(URL))
